4010dd6738b47e577ea2cc2bd8efae99079aeec4
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-07-25 07:37:10 +0700

    Feat(backend): checkCredentials endpoint

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -52,8 +52,40 @@ class AuthController extends Controller
                     "user" => $authRes
                 ]);
             }
+            return response()->json(["message" => "Invalid Credentials"], 401);
         } catch (\Exception $err) {
-            Log::info("Register error => " . $err->getMessage());
+            Log::info("Login error => " . $err->getMessage());
+            return response()->json([
+                "message" => "Something went wrong, please try again later!"
+            ], 500);
+        }
+    }
+
+    public function checkCredentials(Request $request)
+    {
+        $payload = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        try {
+            $user = User::where("email", $payload['email'])->first();
+
+            if ($user) {
+                if (!Hash::check($payload['password'], $user->password)) {
+                    return response()->json(["message" => "Invalid Credentials"], 401);
+                }
+
+                $token = $user->createToken("web")->plainTextToken;
+                $authRes = array_merge($user->toArray(), ["token" => $token]);
+                return response()->json([
+                    "status" => 200,
+                    "message" => "Logged in successfully",
+                ]);
+            }
+            return response()->json(["message" => "Invalid Credentials"], 401);
+        } catch (\Exception $err) {
+            Log::info("Login credentials error => " . $err->getMessage());
             return response()->json([
                 "message" => "Something went wrong, please try again later!"
             ], 500);
