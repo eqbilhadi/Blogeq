ccf0bb1327149c853bf0b0e6548764c7c1817968
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-07-27 17:04:35 +0700

    Feat(backend): create get and post api for posts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +12,11 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
-    Route::post("/update/profile", [UserController::class, 'updateProfileImage']);
+    Route::post('/update/profile', [UserController::class, 'updateProfileImage']);
+
+    Route::apiResources([
+        'post' => PostController::class
+    ]);
 });
 
 Route::post('/auth/register', [AuthController::class, 'register']);
